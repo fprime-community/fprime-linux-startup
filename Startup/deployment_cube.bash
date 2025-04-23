f19aa7a242d2681ab0f5bcb04129d22b17f87ad4
gmiles <gregory.j.miles@jpl.nasa.gov>
2025-04-23 11:17:11 -0700

    Make updates more generic

--- Startup/deployment_cube.bash
+++ Startup/deployment_cube.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DEPLOYMENT=GSAT
+# Change CUBESAT to cubesate deployment name
+DEPLOYMENT=CUBESAT
 
 # boot counter is filled in by expand_fsw_args in startup.bash
 FSW_ARGS="-p 50000 -a 192.168.2.1 -b"
